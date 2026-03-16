d414b2350599856e3d34593874808f0bcf648217
Nikhil Agarwal <agarwalnikhil909@gmail.com>
2026-03-17 00:52:49 +0530

    update metrics file

--- client/lib/server/db/services/metrics.ts
+++ client/lib/server/db/services/metrics.ts
@@ -1,11 +1,21 @@
 import { Metrics } from "../models/metrics";
 
+
 export async function incrementVisits() {
-  await Metrics.findOneAndUpdate({}, { $inc: { visits: 1 } }, { upsert: true, new: true });
+  await Metrics.findOneAndUpdate(
+    {},
+    { $inc: { visits: 1 } },
+    { upsert: true, returnDocument: "after" }
+  );
 }
 
+
 export async function incrementSearches() {
-  await Metrics.findOneAndUpdate({}, { $inc: { searches: 1 } }, { upsert: true, new: true });
+  await Metrics.findOneAndUpdate(
+    {},
+    { $inc: { searches: 1 } },
+    { upsert: true, returnDocument: "after" }
+  );
 }
 
 export async function getMetrics() {
